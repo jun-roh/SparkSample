83117d7579771cdcfe87831c3f537a49442c0251
jun-roh <jun.roh85@gmail.com>
2021-05-22 22:33:38 +0900

    - dependencies 버전 수정   - janino error (원인 수정) - url : /  에서 json file load 해서 dataframe 만드는것 수정

--- src/main/java/com/test/spark/SparkSampleApplication.java
+++ src/main/java/com/test/spark/SparkSampleApplication.java
@@ -19,39 +19,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 @SpringBootApplication
-public class SparkSampleApplication implements CommandLineRunner {
-
-    @Autowired
-    private JavaSparkContext sparkContext;
-
-    @Autowired
-    private SparkSession sparkSession;
-
-    @Autowired
-    private JavaStreamingContext streamingContext;
-
+public class SparkSampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SparkSampleApplication.class, args);
     }
-
-    @Override
-    public void run(String... arg0) throws Exception {
-        sparkContext.parallelize(Arrays.asList(1, 2, 3, 4, 5)).map(eachData -> eachData + 1).collect().stream()
-                .forEach(System.out::println);
-
-        File tempFile = new File("src/main/resources/data.json");
-        boolean exists = tempFile.exists();
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("현재 프로젝트의 경로 : "+exists );
-        System.out.println(sparkSession.read().json("src/main/resources/data.json").count());
-//        System.out.println(sample.count());
-
-//        final Queue<JavaRDD<String>> queue = new LinkedList<JavaRDD<String>>();
-//        queue.add(sparkContext.parallelize(Arrays.asList("1, India, Rupee, Delhi, NarendraModi, Indians, Bollywood", "2, USA, Dollar, WashingtonDC, DonaldTrump, Americans, Hollywood")));
-//        final JavaDStream<String> stream = streamingContext.queueStream(queue);
-//        stream.flatMap(line -> Arrays.asList(line.split(",")).iterator()).mapToPair(word -> new Tuple2<String, Integer>(word, 1)).reduceByKey((val1, val2) -> val1 + val2).print();
-//        streamingContext.start();
-//        streamingContext.awaitTermination();
-    }
-
 }
--- src/main/java/com/test/spark/controller/MainController.java
+++ src/main/java/com/test/spark/controller/MainController.java
@@ -1,15 +1,41 @@
 package com.test.spark.controller;
 
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
+    @Autowired
+    private JavaSparkContext sparkContext;
+
+    @Autowired
+    private SparkSession sparkSession;
+
+    @Autowired
+    private JavaStreamingContext streamingContext;
+
     @GetMapping("/")
     public String index(Model model){
+        // json to spark dataframe
+        Dataset<Row> df =  sparkSession.read().format("json")
+                .option("multiline", true)
+                .load("src/main/resources/data.json");
+        df.show(5, 150);
+        df.printSchema();
+
+        //
         return "index";
     }
 }
